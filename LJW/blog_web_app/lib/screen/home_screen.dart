61b33197f7b4d856e83f8bebf32e76e893ef3818
wooooooongs <pires11233@gmail.com>
2024-04-12 22:39:31 +0900

    🔥  사용하지 않는 웹뷰 코드 삭제

--- LJW/blog_web_app/lib/screen/home_screen.dart
+++ LJW/blog_web_app/lib/screen/home_screen.dart
@@ -54,34 +54,3 @@ class _WebViewAppState extends State<WebViewApp> {
         ));
   }
 }
-
-class HomeScreen extends StatelessWidget {
-  WebViewController webViewController = WebViewController()
-    ..loadRequest(Uri.parse('https://flutter.dev'))
-    ..setJavaScriptMode(JavaScriptMode.unrestricted);
-
-  HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: const Text('웹뷰'),
-          centerTitle: true,
-          actions: [
-            IconButton(
-                onPressed: () {
-                  webViewController
-                      .loadRequest(Uri.parse('http://flutter.dev'));
-                },
-                icon: const Icon(
-                  Icons.home,
-                )),
-          ],
-        ),
-        body: WebViewWidget(
-          controller: webViewController,
-        ));
-  }
-}
